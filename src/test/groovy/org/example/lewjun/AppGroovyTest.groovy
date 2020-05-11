190da73d92a05a870342dc2062676b48f86c18bd
LewJun <lewjun072@sina.com>
2020-05-11 18:03:06 +0800

    变量的使用

---
+++ src/test/groovy/org/example/lewjun/AppGroovyTest.groovy
@@ -0,0 +1,30 @@
+package org.example.lewjun
+
+class GroovyTest extends GroovyTestCase {
+    /**
+     * 变量
+     */
+    void testVar() {
+        def empno = 7399
+        def ename = 'King'
+        def job = "Master"
+        def mgr = 9999
+        def hiredate = new Date()
+        def deptno = 10
+        def sex = true
+        def height = 172.3f
+        // 对于浮点型字面量为了精度 Groovy 默认使用的类型为 BigDecimal
+        def weight = 62.3
+
+        println mgr.getClass() // class java.lang.Integer
+        println height.getClass() // class java.lang.Float
+        println empno instanceof Integer // true
+        println weight.getClass() // class java.math.BigDecimal
+
+        // 在Groovy中，一切皆对象，包括数字也是
+        println 123.floatValue().getClass()
+
+        println 123.plus(2)
+    }
+
+}
